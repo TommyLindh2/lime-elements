2ad49faf0045a7d5d0941507e9cd9b9000256581
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-19 15:20:12 +0100

    ci(semantic-release): enable release branches `master`, `beta`, `staging`, `[d].x`, `[d].[d].x`

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    branches: ['master'],
+    branches: [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'master',
+        { name: 'beta', prerelease: true },
+        { name: 'staging', prerelease: true },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
